b18a0e978c0b1a33c7d9ad00a7ca0d83addd31c6
diditamin.m <disanderlidsbun@gmail.com>
2022-06-28 03:55:35 +0800

    initialize

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,29 @@
+<header>
+    <div class="logo">Chandrakobstan <span>films</span></div>
+    <nav>
+        <ul>
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/">About</a>
+            </li>
+            <li>
+                <a href="/">Journal</a>
+            </li>
+            <li>
+                <a href="/">Contact</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+
+<style>
+    header {
+        position: fixed;
+        top: 0;
+        left: 0;
+        height: 30px;
+        width: 100%;
+}
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import "$lib/css/global.css"
+    import Header from "../lib/components/header.svelte";
+</script>
+
+<main>
+    <Header/>
+    <slot/>
+</main>
+
